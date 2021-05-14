27d784fa8b434e944a94f562ade021df8ede907f
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-05-15 00:06:25 +0300

    sheesh

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Underline } from "./src/Underline";
 
 const ul = new Underline({
-  token: "ODI0MjEwMTMyMzUwMDA5MzY2.YFsDgA.kJAd_AsWcuYGJEvre-_DuimROB8",
+  token: "sheesh",
   prefixes: ["!", "abc!"],
   owners: ["707309693449535599"]
 });
