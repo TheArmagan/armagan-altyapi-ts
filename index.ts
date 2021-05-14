2621b705f465e83bc1ec414d8c03b4c41aa14229
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-05-15 00:46:09 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Underline } from "./src/Underline";
 
 const ul = new Underline({
-  token: "ODI0MjEwMTMyMzUwMDA5MzY2.YFsDgA.wGIWJRK7BhP3Dx4SCjCIFfmXX34",
+  token: "uwijs",
   prefixes: ["!", "abc!"],
   owners: ["707309693449535599"]
 });
