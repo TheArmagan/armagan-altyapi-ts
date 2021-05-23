7db0f9af09ea813a912a76386c963b2be14eec58
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-05-23 23:15:28 +0300

    allow unused variables

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Underline } from "./src/Underline";
 
 const ul = new Underline({
-  token: "ODQ2MTE0NjkyMDY0ODcwNDYx.YKqztg.SCRYDRIbhjxRqKz4zmZtObtqOko",
+  token: "ODQ2MTE0NjkyMDY0ODcwNDYx.YKqztg.3pE1iC879cPOAdORjLjbvJB9v-k",
   prefixes: ["!", "abc!"],
   owners: ["707309693449535599"]
 });
